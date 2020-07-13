4d410888189daf9cd91d03121dc5114b3de8308b
ravi19999 <dahalravindra@gmail.com>
2020-07-13 22:39:40 +0545

    Initialied the express web server :see_no_evil: :zap:

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("ello")
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hi there!');
+});
+
+app.listen(3000, () => {
+    console.log('Listening');
+});
