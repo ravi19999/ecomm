10f08994746ef6690ee8ec865220cbbf36bc12c1
ravi19999 <dahalravindra@gmail.com>
2020-07-15 16:34:30 +0545

    Replaced bodyParser with an external library and used it globally:heavy_plus_sign:

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 
-app.get('/', (req, res) => {
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
     res.send(`
     <div>
         <form method="POST">
@@ -14,30 +18,11 @@ app.get('/', (req, res) => {
     `);
 });
 
-const bodyParser = (req, res, next) => {
-    if (req.method === 'POST') {
-        req.on('data', data => {
-            const parsed = data.toString('utf8').split('&');
-            const formData = {};
-            for (let pair of parsed) {
-                const [key, value] = pair.split('=');
-                formData[key] = value;
-            }
-            req.body = formData;
-            next();
-        });
-    }
-    else {
-        next();
-    }
-};
-
-app.post('/', bodyParser, (req, res) => {
+app.post("/", (req, res) => {
     console.log(req.body);
-    res.send('Account created');
+    res.send("Account created");
 });
 
-
 app.listen(3000, () => {
-    console.log('Listening');
+    console.log("Listening");
 });
