22853ac78c4a6f0ba6eb72915d1be7b5f3682071
ravi19999 <dahalravindra@gmail.com>
2020-07-16 00:17:26 +0545

    Refactored the getAll() function :recycle:

--- repositories/users.js
+++ repositories/users.js
@@ -15,14 +15,14 @@ class UsersRepository {
     }
 
     async getAll() {
-        const contents = await fs.promises.readFile(this.filename, { encoding: 'utf8' });
-        console.log(contents);
+        return JSON.parse(await fs.promises.readFile(this.filename, { encoding: 'utf8' }));
     }
 }
 
 const test = async () => {
     const repo = new UsersRepository('users.json');
-    await repo.getAll();
+    const users = await repo.getAll();
+    console.log(users);
 };
 
 test();
