114b5355e785e8011e7a07b0878ec2c74a144e8d
ravi19999 <dahalravindra@gmail.com>
2020-07-16 22:43:40 +0545

    Generated a random ID and assigned it to every entry :sparkles:

--- repositories/users.js
+++ repositories/users.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const crypto = require("crypto");
 
 class UsersRepository {
   constructor(filename) {
@@ -23,6 +24,7 @@ class UsersRepository {
   }
 
   async create(attrs) {
+    attrs.id = this.randomId();
     const records = await this.getAll();
     records.push(attrs);
 
@@ -34,6 +36,10 @@ class UsersRepository {
       this.filename,
       JSON.stringify(records, null, 2)
     );
+  }
+
+  randomId() {
+    return crypto.randomBytes(4).toString("hex");
   }
 }
 
