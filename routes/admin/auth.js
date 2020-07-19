4332cae0985e58b8ac0113fdee76dd2949aabb64
ravi19999 <dahalravindra@gmail.com>
2020-07-19 22:07:53 +0545

    Added custom validation middleware :sparkles:

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -15,31 +15,33 @@ router.post(
   [
     check("email")
       .trim()
-      .normalizEmail()
-      .isEmail(),
+      .normalizeEmail()
+      .isEmail()
+      .withMessage("Must be a valid email")
+      .custom(async (email) => {
+        const existingUser = await usersRepo.getOneBy({ email });
+        if (existingUser) {
+          throw new Error("Email in use");
+        }
+      }),
     check("password")
       .trim()
-      .isLength({ min: 4, max: 20 }),
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Must be between 4 and 20 characters"),
     check("passwordConfirmation")
       .trim()
-      .isLength({ min: 4, max: 20 }),
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Must be between 4 and 20 characters")
+      .custom((passwordConfirmation, { req }) => {
+        if (passwordConfirmation !== req.body.password) {
+          throw new Error("Passwords must match");
+        }
+      }),
   ],
   async (req, res) => {
     const errors = validationResult(req);
     console.log(errors);
-
     const { email, password, passwordConfirmation } = req.body;
-
-    const existingUser = await usersRepo.getOneBy({ email });
-
-    if (existingUser) {
-      return res.send("Email in use");
-    }
-
-    if (password !== passwordConfirmation) {
-      return res.send("Passwords must match");
-    }
-
     const user = await usersRepo.create({ email, password });
 
     req.session.userId = user.id;
