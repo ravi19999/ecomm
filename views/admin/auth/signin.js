e7e30304d8e0c4718a6ca23907a753595c730f5e
ravi19999 <dahalravindra@gmail.com>
2020-07-19 21:02:11 +0545

    Created the layout file to take care of templates :lipstick: :speech_balloon:

--- views/admin/auth/signin.js
+++ views/admin/auth/signin.js
@@ -1,12 +1,15 @@
-module.exports = () => {
-  return `
-  <div>
-    <form method="POST">
-        <input name="email" placeholder="email" />
-        <input name="password" placeholder="password" />
-        <button>Sign In</button>
-    </form>
-  </div> 
+const layout = require("../layout");
 
-  `;
+module.exports = () => {
+  return layout({
+    content: `
+      <div>
+        <form method="POST">
+            <input name="email" placeholder="email" />
+            <input name="password" placeholder="password" />
+            <button>Sign In</button>
+        </form>
+      </div>
+  `,
+  });
 };
