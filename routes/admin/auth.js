58555e0ec7aa5f3f7ac6d1db72278aeb273f2793
ravi19999 <dahalravindra@gmail.com>
2020-07-21 02:46:25 +0545

    Created a view for addning new products :poop:

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,25 +1,25 @@
-const express = require("express");
-const { check, validationResult } = require("express-validator");
+const express = require('express');
+const { check, validationResult } = require('express-validator');
 
-const usersRepo = require("../../repositories/users");
-const signupTemplate = require("../../views/admin/auth/signup");
-const signinTemplate = require("../../views/admin/auth/signin");
+const usersRepo = require('../../repositories/users');
+const signupTemplate = require('../../views/admin/auth/signup');
+const signinTemplate = require('../../views/admin/auth/signin');
 const {
   requireEmail,
   requirePassword,
   requirePasswordConfirmation,
   requireEmailExists,
-  requireValidPasswordForUser,
-} = require("./validators");
+  requireValidPasswordForUser
+} = require('./validators');
 
 const router = express.Router();
 
-router.get("/signup", (req, res) => {
+router.get('/signup', (req, res) => {
   res.send(signupTemplate({ req }));
 });
 
 router.post(
-  "/signup",
+  '/signup',
   [requireEmail, requirePassword, requirePasswordConfirmation],
   async (req, res) => {
     const errors = validationResult(req);
@@ -33,21 +33,21 @@ router.post(
 
     req.session.userId = user.id;
 
-    res.send("Account created!!!");
+    res.send('Account created!!!');
   }
 );
 
-router.get("/signout", (req, res) => {
+router.get('/signout', (req, res) => {
   req.session = null;
-  res.send("You are logged out");
+  res.send('You are logged out');
 });
 
-router.get("/signin", (req, res) => {
+router.get('/signin', (req, res) => {
   res.send(signinTemplate({}));
 });
 
 router.post(
-  "/signin",
+  '/signin',
   [requireEmailExists, requireValidPasswordForUser],
   async (req, res) => {
     const errors = validationResult(req);
@@ -62,7 +62,7 @@ router.post(
 
     req.session.userId = user.id;
 
-    res.send("You are signed in!!!");
+    res.send('You are signed in!!!');
   }
 );
 
