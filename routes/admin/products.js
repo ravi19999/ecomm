6c4fe517eac8d381553665724ce1e3de8a1df524
ravi19999 <dahalravindra@gmail.com>
2020-07-21 23:15:25 +0545

    Installed the multer library to handle file uploads :heavy_plus_sign: :sparkles:

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const { validationResult } = require("express-validator");
+const multer = require("multer");
+
 const productsRepo = require("../../repositories/products");
 const productsNewTemplate = require("../../views/admin/products/new");
 const { requireTitle, requirePrice } = require("./validators");
 
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.get("/admin/products", (req, res) => {});
 
@@ -12,13 +15,16 @@ router.get("/admin/products/new", (req, res) => {
   res.send(productsNewTemplate({}));
 });
 
-router.post("/admin/products/new", [requireTitle, requirePrice], (req, res) => {
-  const errors = validationResult(req);
+router.post(
+  "/admin/products/new",
+  [requireTitle, requirePrice],
+  upload.single("image"),
+  (req, res) => {
+    const errors = validationResult(req);
 
-  req.on("data", (data) => {
-    console.log(data.toString());
-  });
-  res.send("submitted");
-});
+    console.log(req.file);
+    res.send("submitted");
+  }
+);
 
 module.exports = router;
