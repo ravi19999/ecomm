c1c75e92154dcde856c5f14fb79e2e8ac205b1fe
ravi19999 <dahalravindra@gmail.com>
2020-07-23 14:09:28 +0545

    Displayed error messages upon edit :bug:

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -35,11 +35,11 @@ router.post(
   }
 );
 
-router.get("/admin/admin/products/:id/edit", async (req, res) => {
+router.get("/admin/products/:id/edit", requireAuth, async (req, res) => {
   const product = await productsRepo.getOne(req.params.id);
 
   if (!product) {
-    return res.send("Product not found!.");
+    return res.send("Product not found");
   }
 
   res.send(productsEditTemplate({ product }));
@@ -50,9 +50,13 @@ router.post(
   requireAuth,
   upload.single("image"),
   [requireTitle, requirePrice],
-  handleErrors(productsEditTemplate),
+  handleErrors(productsEditTemplate, async (req) => {
+    const product = await productsRepo.getOne(req.params.id);
+    return { product };
+  }),
   async (req, res) => {
     const changes = req.body;
+
     if (req.file) {
       changes.image = req.file.buffer.toString("base64");
     }
@@ -63,7 +67,8 @@ router.post(
       return res.send("Could not find item");
     }
 
-    res.redirect("/admin/products");
+    res.redirect("/admin/admin/products");
   }
 );
+
 module.exports = router;
