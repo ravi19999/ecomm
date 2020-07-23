c75d91e941a41fd1cbf898ce1a83bc687c75ee3e
ravi19999 <dahalravindra@gmail.com>
2020-07-23 22:33:34 +0545

    Hooked up the carts router :sparkles:

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cookieSession = require("cookie-session");
 const authRouter = require("./routes/admin/auth");
 const adminProductsRouter = require("./routes/admin/products");
 const productsRouter = require("./routes/products");
+const cartsRouter = require("./routes/carts");
+
 const app = express();
 
 app.use(express.static("public"));
@@ -16,6 +18,8 @@ app.use(
 app.use(authRouter);
 app.use(productsRouter);
 app.use(adminProductsRouter);
+app.use(cartsRouter);
+
 app.listen(3000, () => {
   console.log("Listening");
 });
