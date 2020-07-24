16e89f27934f24c69d220967d3e0cbb8ac43cde5
ravi19999 <dahalravindra@gmail.com>
2020-07-24 21:35:42 +0545

    Added items to the cart :sparkles:

--- routes/carts.js
+++ routes/carts.js
@@ -5,13 +5,25 @@ const router = express.Router();
 router.post("/cart/products", async (req, res) => {
   let cart;
   if (!req.session.cartId) {
-    cart = await cartsRepo.create({ items: {} });
+    cart = await cartsRepo.create({ items: [] });
     req.session.cartId = cart.id;
   } else {
     cart = await cartsRepo.getOne(req.session.cartId);
   }
 
-  console.log(cart);
+  const existingItem = cart.items.find(
+    (item) => item.id === req.body.productId
+  );
+  if (existingItem) {
+    existingItem.quantity++;
+  } else {
+    cart.items.push({ id: req.body.productId, quantity: 1 });
+  }
+  await cartsRepo.update(cart.id, {
+    items: cart.items,
+  });
+
+  res.send("Product added to cart");
 
   res.send("Product added to cart");
 });
